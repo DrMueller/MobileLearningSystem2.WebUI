9e59edf445aa844813903a8a2333a7b98b803bf9
Matthias Mueller <matthiasm@live.de>
2018-04-03 19:37:00 +0200

    WIP

--- src/app/infrastructure/aspects/app-navigation/components/app-navigation/app-navigation.component.ts
+++ src/app/infrastructure/aspects/app-navigation/components/app-navigation/app-navigation.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+    AppInfoDataService, ApplicationInformation
+} from 'app/infrastructure/core-services/app-info';
+
 import { AppNavigationEntry } from '../../models';
 import { AppNavigationService } from '../../services';
-
-import { AppInfoDataService, ApplicationInformation } from 'app/infrastructure/core-services/app-info';
 
 @Component({
   selector: 'app-app-navigation',
@@ -15,12 +17,12 @@ export class AppNavigationComponent implements OnInit {
   public applicationInformation: ApplicationInformation
   public appNavigationEntries: AppNavigationEntry[] = [];
 
-  constructor(
-    private appNavigationService: AppNavigationService,
-    private appInfoDataService: AppInfoDataService) { }
-
   public async ngOnInit(): Promise<void> {
     this.appNavigationEntries = this.appNavigationService.getNavigationEntries();
     this.applicationInformation = await this.appInfoDataService.getApplicationInformationAsync();
   }
+
+  constructor(
+    private appNavigationService: AppNavigationService,
+    private appInfoDataService: AppInfoDataService) { }
 }
